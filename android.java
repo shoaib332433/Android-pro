89bcd2d6c9fef7602acbcafe486f2dc8ce1b134f
shoaib332433 <shoaib.shah660@gmail.com>
2019-10-23 10:44:37 +0500

    This is new Commit

---
+++ android.java
@@ -0,0 +1,14 @@
+public void main(String[] arr){
+    System.out.println("Hello World");
+
+    Button bnt=findViewById(R.id.new);
+
+    btn.setOnclickListner(New View.OnclickListner{
+
+        @Override
+            public void Onclick(){
+
+        }
+
+        })
+}
